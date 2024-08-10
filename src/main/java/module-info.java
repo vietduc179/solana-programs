5630fe4817af699235d4455336580fdd199ea96b
jpe7s <jpe7s.salt188@passfwd.com>
2024-08-10 16:32:40 -0500

    export system program.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,10 +6,11 @@ module software.sava.solana_programs {
   requires software.sava.core;
   requires software.sava.rpc;
 
+  exports software.sava.solana.programs.address_lookup_table;
   exports software.sava.solana.programs.clients;
+  exports software.sava.solana.programs.compute_budget;
   exports software.sava.solana.programs.stake;
   exports software.sava.solana.programs.stakepool;
+  exports software.sava.solana.programs.system;
   exports software.sava.solana.programs.token;
-  exports software.sava.solana.programs.compute_budget;
-  exports software.sava.solana.programs.address_lookup_table;
 }
