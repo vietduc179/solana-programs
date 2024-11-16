a5007f2fd8a74f75b2fdfe2734ea736a8fd8350d
jpe7s <jpe7s.salt188@passfwd.com>
2024-11-16 08:21:36 -0600

    Add compute budget setLoadedAccountsDataSizeLimit ix.

--- src/main/java/software/sava/solana/programs/compute_budget/ComputeBudgetProgram.java
+++ src/main/java/software/sava/solana/programs/compute_budget/ComputeBudgetProgram.java
@@ -9,8 +9,20 @@ import static software.sava.core.tx.Instruction.createInstruction;
 
 public final class ComputeBudgetProgram {
 
-  public static int COMPUTE_UNITS_CONSUMED = 300;
+  public static int CU_LIMIT_CONSUMPTION = 150;
+  public static int CU_PRICE_CONSUMPTION = 150;
+  // CU's consumed for typical inclusion of set CU limit and price.
+  public static int COMPUTE_UNITS_CONSUMED = CU_LIMIT_CONSUMPTION + CU_PRICE_CONSUMPTION;
+  public static int SET_LOADED_ACCOUNT_SIZE_LIMIT_CONSUMPTION = 150;
+
   public static int MAX_COMPUTE_BUDGET = 1_400_000;
+
+  public static Instruction requestHeapFrame(final AccountMeta invokedProgram, final int heapRegionSize) {
+    final byte[] data = new byte[5];
+    data[0] = (byte) 1;
+    ByteUtil.putInt32LE(data, 1, heapRegionSize);
+    return createInstruction(invokedProgram, NO_KEYS, data);
+  }
 
   public static Instruction setComputeUnitLimit(final AccountMeta invokedProgram, final int units) {
     final byte[] data = new byte[5];
@@ -23,6 +35,13 @@ public final class ComputeBudgetProgram {
     final byte[] data = new byte[9];
     data[0] = (byte) 3;
     ByteUtil.putInt64LE(data, 1, microLamports);
+    return createInstruction(invokedProgram, NO_KEYS, data);
+  }
+
+  public static Instruction setLoadedAccountsDataSizeLimit(final AccountMeta invokedProgram, final int limit) {
+    final byte[] data = new byte[5];
+    data[0] = (byte) 4;
+    ByteUtil.putInt32LE(data, 1, limit);
     return createInstruction(invokedProgram, NO_KEYS, data);
   }
 
